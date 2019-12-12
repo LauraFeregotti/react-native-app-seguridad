6a4dd21780f7ecf06fb4d4d2d801ab4edd002775
Nico Ramirez <patyattack@MacBook-Pro-de-Nico.local>
2019-12-11 21:31:49 -0300

    estilos mapa textp

--- screens/MapaDeEmergencia.js
+++ screens/MapaDeEmergencia.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from "react-native";
 const MapaDeEmergencia = props => {
      return (
         <View style={styles.screen}>
-            <Text>
+            <Text style={styles.texto}>
              Mapa de Emergencia
             </Text>
     </View>
@@ -16,12 +16,19 @@ MapaDeEmergencia.navigationOptions = {
 };
 
 const styles = StyleSheet.create({
-    screen: {
+  screen: {
     flex: 1,
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center"
-  }
+  },
+  texto: {
+      color: "#005b5c",
+      marginLeft: 25,
+      fontSize: 20,
+      fontWeight: 'bold',
+      fontFamily: "roboto"
+    }
 });
 
 export default MapaDeEmergencia;
